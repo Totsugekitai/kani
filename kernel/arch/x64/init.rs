9ddfe5a7f1e6e1b555ee3c16af8929bd61d968b5
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-11-08 12:49:47 +0900

    tmp

--- kernel/arch/x64/init.rs
+++ kernel/arch/x64/init.rs
@@ -1,8 +1,11 @@
+#![feature(llvm_asm)]
+
 use crate::arch::x64::uart;
 
 #[no_mangle]
 #[warn(dead_code)]
 unsafe extern "C" fn init_x86() {
+    llvm_asm!("mov dword ptr [0xb8000], 0x2f4b2f4f" :::: "intel");
     let _ = uart::init();
     for _ in 0..5 {
         let mut c = b'A';
