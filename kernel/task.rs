f26788456fecdf9007c57d7fa8b2fd4b2310096f
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-11-20 11:28:24 +0900

    wip

--- kernel/task.rs
+++ kernel/task.rs
@@ -1,58 +1,32 @@
 use crate::arch::task::ContextX64;
-use crossbeam::queue::ArrayQueue;
+use core::sync::atomic::AtomicU64;
 use lazy_static::lazy_static;
-use spin::mutex::Mutex;
 
+#[cfg(target_arch = "x86_64")]
 #[derive(Debug, Clone, Copy)]
 #[repr(C, align(16))]
 pub struct Task {
     pub ctx: ContextX64,
-}
-
-const NUM_TASKS: usize = 100;
-
-lazy_static! {
-    static ref TASK_QUEUE: ArrayQueue<Task> = ArrayQueue::new(NUM_TASKS);
-    static ref CURRENT_TASK: Mutex<Task> = Mutex::new(Task::empty());
+    idx: u64,
 }
 
 impl Task {
     pub fn new(f: u64, stack_bottom: u64) -> Self {
         Self {
             ctx: ContextX64::new(f, stack_bottom),
+            idx: 0,
         }
-    }
-
-    pub fn empty() -> Self {
-        Self {
-            ctx: ContextX64::new(Task::empty_fn as u64, 0),
-        }
-    }
-
-    fn empty_fn() {}
-
-    pub fn register(self) {
-        push_task_queue(self);
     }
 }
 
-fn push_task_queue(task: Task) {
-    TASK_QUEUE.push(task);
-}
+const NUM_TASKS: usize = 0x100;
+static TASK_ARRAY: [Option<Task>; NUM_TASKS] = [None; NUM_TASKS];
 
-fn select_next_task() -> Option<Task> {
-    TASK_QUEUE.pop()
+lazy_static! {
+    static ref CURRENT_TASK_INDEX: AtomicU64 = AtomicU64::new(0);
 }
 
 pub fn switch_task(current_task: &Task, next_task: &Task) {
-    // let current_task = *CURRENT_TASK.lock();
-    // let next_task = if let Some(t) = select_next_task() {
-    //     t
-    // } else {
-    //     current_task
-    // };
-    // push_task_queue(current_task);
-
     unsafe {
         crate::arch::x64::task::switch_context(&current_task.ctx, &next_task.ctx);
     }
